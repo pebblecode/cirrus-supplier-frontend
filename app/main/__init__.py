bb2740d72c533b7e391cabe67a28dea3555ba3b0
Jonathan King <jonathan@Jonathans-Macintosh.local>
2016-06-06 15:33:50 +0100

    Finally found the issue with the content loader, the slug was wrong

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -22,10 +22,10 @@ content_loader.load_manifest('g-cloud-8', 'services', 'edit_submission')
 content_loader.load_manifest('g-cloud-8', 'declaration', 'declaration')
 content_loader.load_messages('g-cloud-8', ['dates'])
 
-content_loader.load_manifest('cirrus', 'services', 'edit_service')
-content_loader.load_manifest('cirrus', 'services', 'edit_submission')
-content_loader.load_manifest('cirrus', 'declaration', 'declaration')
-content_loader.load_messages('cirrus', ['dates'])
+content_loader.load_manifest('cirrus-1', 'services', 'edit_service')
+content_loader.load_manifest('cirrus-1', 'services', 'edit_submission')
+content_loader.load_manifest('cirrus-1', 'declaration', 'declaration')
+content_loader.load_messages('cirrus-1', ['dates'])
 
 
 @main.after_request
